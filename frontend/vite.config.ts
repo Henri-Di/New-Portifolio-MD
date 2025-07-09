d31f43852ca71ca57341d2a66702cb6fed91e61f
Matheus Diamantino <matheus.diamantino20@gmail.com>
2025-07-09 00:09:07 -0300

    fix: Correção e atualização do Vite para deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT ?? '10000'),
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    proxy: mode === 'development' ? {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+    } : undefined,
   },
-})
+}))
